72f760033e776ae3af716abf4a46e3e4c1c1b062
Pedro Pauluci <pauluci125romao.pedro@hotmail.com>
2021-04-27 08:49:49 -0300

    primeira aula

--- cademeumedico.com.br/public_html/config/web.php
+++ cademeumedico.com.br/public_html/config/web.php
@@ -16,7 +16,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'DXXsMejp-gmxknJ_UTGKmKBhvkhIsJoT',
+            'cookieValidationKey' => 'hBZaSLXh2B9e-48t4RTjjGHuRPC7Ppcm',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
--- cademeumedico.com.br/public_html/views/site/about.php
+++ cademeumedico.com.br/public_html/views/site/about.php
@@ -5,14 +5,18 @@
 use yii\helpers\Html;
 
 $this->title = 'Sobre-nos';
+
 $this->params['breadcrumbs'][] = $this->title;
+
 $this->registerCss("h1 {font-size:78px}");
+
 ?>
+
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        Essa é uma pagina que fala um pouco sobre nós.
+        Nesta pagina vamos falar um pouco sobre nós
     </p>
 
     <code><?= __FILE__ ?></code>
